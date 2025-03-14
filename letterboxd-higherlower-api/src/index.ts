9d6d5b5e7ddcc7e6189895b0cbec1d791eacbc55
Jonathan Marcantonio <jmarcant@redhat.com>
2025-03-14 10:23:53 -0400

    movie csv to db
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- letterboxd-higherlower-api/src/index.ts
+++ letterboxd-higherlower-api/src/index.ts
@@ -45,8 +45,72 @@ export default {
 			});
 		}
 
+		// if (pathname === "/api/csv") {
+		// 	try {
+		// 		const formData = await request.formData();
+		// 		const csvFile = formData.get("file");
+		
+		// 		if (!csvFile || !(csvFile instanceof File)) {
+		// 		  return new Response("No file uploaded or invalid file", { status: 400 });
+		// 		}
+		
+		// 		const csvText = await csvFile.text();
+				
+		// 		const parsedData = parseCSV(csvText);
+			  
+		// 		// Insert parsed data into the D1 database
+		// 		for (const row of parsedData) {
+		// 			const { title, movie_url, poster_url, rating, genre } = row;
+		// 			await env.DB.prepare(
+		// 			`
+		// 			INSERT INTO Movies (movie_name, rating, poster_url, movie_url, genre)
+		// 			VALUES (?, ?, ?, ?, ?)
+		// 			`
+		// 			)
+		// 			.bind(title, parseFloat(rating), poster_url, movie_url, genre)
+		// 			.run();
+		// 		}
+	  
+		// 	  return new Response("CSV data inserted successfully!", {
+		// 		headers: { ...corsHeaders },
+		// 	  });
+		// 	} catch (error) {
+		// 	  return new Response(`Error processing CSV: ${error}`, {
+		// 		status: 500,
+		// 		headers: { ...corsHeaders },
+		// 	  });
+		// 	}
+		//   }
+	  
+
 		return new Response(
 			"Call it.",
 		);
 	},
 } satisfies ExportedHandler<Env>;
+
+// function parseCSV(csvText: string): Array<{ title: string, movie_url: string, poster_url: string, rating: string, genre: string }> {
+// 	const rows = csvText.split("\n"); // Split into rows
+// 	console.log(rows[3])
+// 	let headers = rows[0].split(",")
+// 	headers[4] = "genre";
+// 	console.log(headers)
+// 	const data = rows.slice(1).map((row) => {
+		
+// 	  const columns = row.split(",");
+// 	  const rowObject: any = {};
+  
+// 	  columns.forEach((value, index) => {
+// 		const cleanedValue = value.trim().replace(/\r/g, ""); // remove carriage return
+
+// 		if (headers[index] === "rating") {
+// 			console.log(cleanedValue)
+// 		}
+// 		rowObject[headers[index]] = cleanedValue;
+// 	  });
+// 	  return rowObject;
+// 	});
+  
+// 	return data;
+//   }
+  
