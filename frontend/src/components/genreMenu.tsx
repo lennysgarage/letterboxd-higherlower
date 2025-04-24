62a8fe770791d945535fe8b9924b15917448848e
Jonathan Marcantonio <lennysgarage1@gmail.com>
2025-04-23 22:27:08 -0400

    Add missing comedy genre to menu (#1)
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- frontend/src/components/genreMenu.tsx
+++ frontend/src/components/genreMenu.tsx
@@ -7,7 +7,7 @@ interface GenreMenu {
 }
 
 const GenreMenu: React.FC<GenreMenu> = ({ selectedGenres, setSelectedGenres }) => {
-    const availableGenres = ['action', 'adventure', 'animation', 'crime', 'documentary', 'drama', 'family', 'fantasy', 'history', 'horror', 'music', 'mystery', 'romance', 'science-fiction', 'thriller', 'war', 'western'];
+    const availableGenres = ['action', 'adventure', 'animation', 'comedy', 'crime', 'documentary', 'drama', 'family', 'fantasy', 'history', 'horror', 'music', 'mystery', 'romance', 'science-fiction', 'thriller', 'war', 'western'];
 
     const [selection, setSelection] = useState("All Genres");
 
